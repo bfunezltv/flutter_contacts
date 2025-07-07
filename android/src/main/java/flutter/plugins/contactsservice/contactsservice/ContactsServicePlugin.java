38c5c9db782aa1f5834b523210a3b0808c58377a
Brayan Funez <bfunez@ltvco.com>
2025-07-07 14:30:15 -0600

    remove registrar import

--- android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
+++ android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
@@ -41,7 +41,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.provider.ContactsContract.CommonDataKinds;
